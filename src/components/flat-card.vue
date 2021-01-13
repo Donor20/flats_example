c86795b9122829385b21ddeed6caa5431616e181
Donor20 <thisisstory@mail.ru>
2021-01-13 18:47:06 +0300

    Start

---
+++ src/components/flat-card.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-card>
+    <q-card-section>
+      {{flat}}
+      <div class="row">
+        <div style="opacity: 50%">{{flat.porch}} этаж</div>
+        <div style="flex: 1;"></div>
+        {{flat.rooms}} {{getRoomWord(flat.rooms)}} - {{flat.square}} м²
+      </div>
+      <div class="items-center" style="border: 1px solid #EBEBEB;border-radius: 5px; width: 250px; height: 250px;">
+        <q-img :src="`${flat.plan}`"></q-img>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'flat-card',
+  props: ['flat'],
+  data () {
+    return { }
+  },
+  methods: {
+    getRoomWord (cnt) {
+      const getWord = (n, w1, w2, w5) => {
+        let tmp = n % 100
+        if (tmp >= 5 && tmp <= 20) return w5
+        tmp = tmp % 10
+        if (tmp === 1) return w1
+        if (tmp >= 2 && tmp <= 4) return w2
+        return w5
+      }
+      return getWord(cnt, 'комната', 'комнаты', 'комнат')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,35 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <div class="row">
+      <flat-card v-for="flat in flats" :key="flat.building_id + '_' + flat.id"
+                 class="my-card col-3"
+                 :flat="flat"
+      />
+    </div>
   </q-page>
 </template>
 
 <script>
+import api from '../services/api'
+import FlatCard from '../components/flat-card'
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: { FlatCard },
+  data () {
+    return {
+      flats: []
+    }
+  },
+  methods: {
+    getFlats () {
+      return api.getFlats().then(flats => {
+        this.flats = flats
+      })
+    }
+  },
+  mounted () {
+    this.getFlats()
+  }
 }
 </script>
